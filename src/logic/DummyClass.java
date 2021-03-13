5f57ee7f47cb6280c42845b04739707189f60da9
pierciro <piercirocaliandro998@gmail.com>
2021-03-13 10:27:24 +0100

    removed DummyClass fo inutility

--- src/logic/DummyClass.java
+++
@@ -1,7 +0,0 @@
-package logic;
-
-public class DummyClass {
-	public void dummyMethod() {
-		System.out.println("Hello World !");
-	}
-}
--- src/logic/Main.java
+++ src/logic/Main.java
@@ -1,9 +1,7 @@
 package logic;
-import logic.DummyClass;
 
 public class Main {
 	public static void main(String[] args) {
-		DummyClass dc = new DummyClass();
-		dc.dummyMethod();
+		System.out.println("Hello Wolrd !");
 	}
 }
